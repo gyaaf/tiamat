1912aa508c944e66525eba3d2ecdb4a0ce721737
Gabriel <49964470+gyaaf@users.noreply.github.com>
2024-10-25 18:59:05 -0300

    lowered delay and fixed crash

--- AutoAccept.py
+++ AutoAccept.py
@@ -12,38 +12,22 @@ class autoaccept:
         state = "ON" if self.auto_accept_enabled else "OFF"
         print(f"Auto accept is now {state}.")
 
-    def accept_match(self, match_id):
-        # Endpoint para aceitar partida
+    def accept_match(self):
         response = self.rengar.lcu_request("POST", f"/lol-matchmaking/v1/ready-check/accept", "")
-        #if response.status_code == 200:
-        #    print(f"\nAccepted match\n")
-        #else:
-        #    print(f"Failed to accept match with ID: {match_id}. Status code: {response.status_code}")
 
-    def auto_accept_matches(self):
+    def monitor_queue(self):
         while True:
             if self.auto_accept_enabled:
-                # Faz a requisição para verificar o estado da busca por partida
                 response = self.rengar.lcu_request("GET", "/lol-lobby/v2/lobby/matchmaking/search-state", "")
                 
                 if response.status_code == 200:
                     match_data = response.json()
                     
-                    # Exibe o conteúdo da resposta para verificar o estado do matchmaking
-                    #print("Matchmaking Data:", match_data)
-
-                    # Verifica se o status indica que o jogador está em um ready-check
                     if match_data.get("searchState") == "Found":
-                    #    print("Match found, ready to accept.")
-                        self.accept_match(None)  # Não há um ID de partida, basta aceitar
-                    #else:
-                    #    print(f"Current search state: {match_data.get('searchState')}")
+                        self.accept_match() 
                 elif response.status_code == 404:
-                    # Estado 404 é esperado quando não há uma busca ativa, então evitamos o spam de erro
                     print("No active matchmaking search.")
                 else:
                     print(f"Failed to fetch matchmaking state. Status code: {response.status_code}")
             
-            time.sleep(1)  # Checa a cada 5 segundos
-
-
+            time.sleep(1)
--- InstalockAutoban.py
+++ InstalockAutoban.py
@@ -51,78 +51,79 @@ class InstalockAutoban:
         """
         while True:
             # print("Checking for Champion Select...")
-            champ_select_resp = self.rengar.lcu_request("GET", "/lol-champ-select/v1/session", "")
+            try:
+                champ_select_resp = self.rengar.lcu_request("GET", "/lol-champ-select/v1/session", "")
+                if "RPC_ERROR" not in champ_select_resp.text:
 
-            if "RPC_ERROR" not in champ_select_resp.text:
+                    root_champ_select = champ_select_resp.json()
+                    cell_id = root_champ_select.get("localPlayerCellId")
 
-                root_champ_select = champ_select_resp.json()
-                cell_id = root_champ_select.get("localPlayerCellId")
-
-                if cell_id == None:
-                    print("Local player ID not found.")
-                    time.sleep(1)
-                    continue
-
-                for actions in root_champ_select["actions"]:
-                    if not isinstance(actions, list):
+                    if cell_id == None:
+                        #print("Local player ID not found.")
+                        time.sleep(0.3)
                         continue
-                    for action in actions:
-                        if self.instalock_enabled and action["actorCellId"] == cell_id and action["type"] == "pick" and not action["completed"]:
-                            
-                            time.sleep(random.uniform(0.5, 1.5))  # Simulando um pequeno delay
 
-                            if self.instalock_champion == "Random":
-                                instalock_champs = self.champion_converter.get_instalock_champs()
-                                champion_id = random.choice(instalock_champs)[0]
-                            else:
-                                champion_id = self.champion_converter.champ_name_to_id(self.instalock_champion)
-
-                            # print(f"Picking champion {self.instalock_champion} (ID: {champion_id})")
-
-                            patch_resp = self.rengar.lcu_request(
-                                "PATCH",
-                                f"/lol-champ-select/v1/session/actions/{action['id']}",
-                                {"completed": True, "championId": champion_id}
-                            )
-
-                            # if patch_resp.status_code == 204:  # 204 = sucesso sem conteúdo
-                            #     print(f"Champion {self.instalock_champion} selected successfully.")
-                            # else:
-                            #     print(f"Failed to select champion. Status code: {patch_resp.status_code}")
-                            # continue  # Após o pick, interrompe o loop para evitar múltiplos picks
-
-                            time.sleep(1)  # Verifica o estado novamente após uma pausa
-
-
-                        elif self.auto_ban_enabled and action["actorCellId"] == cell_id and action["type"] == "ban" and not action["completed"]:
-                            time.sleep(random.uniform(0.5, 1.5))  # Simulando um pequeno delay
-                            champion_id = self.champion_converter.champ_name_to_id(self.auto_ban_champion)
-
-                            print(f"Banning champion {self.auto_ban_champion} (ID: {champion_id})")
-
-                            patch_resp = self.rengar.lcu_request(
-                                "PATCH",
-                                f"/lol-champ-select/v1/session/actions/{action['id']}",
-                                {"completed": True, "championId": champion_id}
-                            )
-
-                            # if patch_resp.status_code == 204:
-                            #     print(f"Champion {self.auto_ban_champion} banned successfully.")
-                            # else:
-                            #     pass
-                            #      print(f"Failed to ban champion. Status code: {patch_resp.status_code}")
+                    for actions in root_champ_select["actions"]:
+                        if not isinstance(actions, list):
                             continue
+                        for action in actions:
+                            if self.instalock_enabled and action["actorCellId"] == cell_id and action["type"] == "pick" and not action["completed"]:
+                                
+                                time.sleep(0.3)  # Simulando um pequeno delay
+
+                                if self.instalock_champion == "Random":
+                                    instalock_champs = self.champion_converter.get_instalock_champs()
+                                    champion_id = random.choice(instalock_champs)[0]
+                                else:
+                                    champion_id = self.champion_converter.champ_name_to_id(self.instalock_champion)
+
+                                # print(f"Picking champion {self.instalock_champion} (ID: {champion_id})")
+
+                                patch_resp = self.rengar.lcu_request(
+                                    "PATCH",
+                                    f"/lol-champ-select/v1/session/actions/{action['id']}",
+                                    {"completed": True, "championId": champion_id}
+                                )
+
+                                # if patch_resp.status_code == 204:  # 204 = sucesso sem conteúdo
+                                #     print(f"Champion {self.instalock_champion} selected successfully.")
+                                # else:
+                                #     print(f"Failed to select champion. Status code: {patch_resp.status_code}")
+                                # continue  # Após o pick, interrompe o loop para evitar múltiplos picks
+
+                                time.sleep(0.3)  # Verifica o estado novamente após uma pausa
 
 
-                            time.sleep(1)  # Verifica o estado novamente após uma pausa
+                            elif self.auto_ban_enabled and action["actorCellId"] == cell_id and action["type"] == "ban" and not action["completed"]:
+                                time.sleep(0.3)  # Simulando um pequeno delay
+                                champion_id = self.champion_converter.champ_name_to_id(self.auto_ban_champion)
+
+                                #print(f"Banning champion {self.auto_ban_champion} (ID: {champion_id})")
+
+                                patch_resp = self.rengar.lcu_request(
+                                    "PATCH",
+                                    f"/lol-champ-select/v1/session/actions/{action['id']}",
+                                    {"completed": True, "championId": champion_id}
+                                )
+
+                                # if patch_resp.status_code == 204:
+                                #     print(f"Champion {self.auto_ban_champion} banned successfully.")
+                                # else:
+                                #     pass
+                                #      print(f"Failed to ban champion. Status code: {patch_resp.status_code}")
+                                continue
 
 
-                # print("Champion Select found, running Instalock and AutoBan if enabled...")
-            else:
+                                time.sleep(0.3)  # Verifica o estado novamente após uma pausa
+
+
+                    # print("Champion Select found, running Instalock and AutoBan if enabled...")
+                else:
+                    pass
+                    # print("Not in Champion Select phase.")
+                time.sleep(0.3)  # Verifica a cada 5 segundos
+            except:
                 pass
-                # print("Not in Champion Select phase.")
-
-            time.sleep(3)  # Verifica a cada 5 segundos
 
     def toggle_instalock(self):
         """
